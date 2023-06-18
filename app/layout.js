4ce87700e2b9624db553ff9723f2779ae843cae1
ranjithmano13 <ranjithmano13@gmail.com>
2023-06-18 18:03:35 +0530

    components and header design

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import "@styles/globals.css";
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+export const metadata = {
+  title: "Promptia",
+  description: "Discover & Share AI Prompts",
+};
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+            {children}
+            <Nav/>
+
+        </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
